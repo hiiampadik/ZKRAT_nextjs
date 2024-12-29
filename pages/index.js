8bcf9b5682485a665242ba7d73a3b4246bccd30b
Bronislav Musil <brona.musil@gmail.com>
2024-12-29 18:37:06 +0100

    Fafde

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import styles from "../styles/Home.module.scss";
 import Layout from "../components";
+import {Fade} from "react-awesome-reveal";
 
 export default function Home() {
 
   return (
-    <Layout>
-      <div className={styles.homepageTitle}>
-          <a href='https://instagram.com/zkrat.kolektiv'>
-            <h1>@zkrat.kolektiv</h1>
-            <span className={styles.ig}>IG</span>
-          </a>
-          <p>
-              Interactive installations
-          </p>
-          <p>
-              Visualizations
-          </p>
-        </div>
-
-    </Layout>
+    <Fade delay={0.5}>
+        <Layout>
+              <div className={styles.homepageTitle}>
+                  <a href='https://instagram.com/zkrat.kolektiv'>
+                    <h1>
+                        @zkrat.kolektiv
+                    </h1>
+                    <span className={styles.ig}>IG</span>
+                  </a>
+                  <p>
+                      Interactive installations
+                  </p>
+                  <p>
+                      Visualizations
+                  </p>
+                </div>
+        </Layout>
+    </Fade>
   );
 }
