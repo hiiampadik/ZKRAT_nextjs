c9990e1815ebd0c5a8c71c7f63a242412e177e09
Bronislav Musil <brona.musil@gmail.com>
2024-12-29 21:14:28 +0100

    Favicons

--- components/index.js
+++ components/index.js
@@ -64,13 +64,12 @@ export default function Layout(
                 <meta name="twitter:title" content={title}/>
                 <meta name="twitter:description" content={description}/>
 
-                <link rel="icon" href="/favicon.ico"/>
-
-                {/*<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96"/>*/}
-                {/*<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg"/>*/}
-                {/*<link rel="shortcut icon" href="/favicon/favicon.ico"/>*/}
-                {/*<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>*/}
-                {/*<meta name="apple-mobile-web-app-title" content={title}/>*/}
+                <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96"/>
+                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg"/>
+                <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+                <meta name="apple-mobile-web-app-title" content="Zkrat Kol."/>
+                <link rel="manifest" href="/favicon/site.webmanifest" />
             </Head>
 
             <script
